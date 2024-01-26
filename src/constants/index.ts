c250c252c88b01e6edd866355508c424972b5d1d
Barbara Tsitsani <barbara.tsitsani@gmail.com>
2024-01-26 17:24:01 +0200

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export { countries } from './country.constant';
+export { regions } from './region.constant';
+export { manufacturers } from './manufacturer.constant';
+export { ALPHABET_CHARS, ALPHABET_INDEX } from './alphabet.constant';
---
+++ src/constants/region.constant.ts
@@ -0,0 +1,40 @@
+export enum regions {
+  'A' = 'Africa',
+  'B' = 'Africa',
+  'C' = 'Africa',
+  'J' = 'Asia',
+  'K' = 'Asia',
+  'L' = 'Asia',
+  'M' = 'Asia',
+  'N' = 'Asia',
+  'O' = 'Asia',
+  'P' = 'Asia',
+  'Q' = 'Asia',
+  'R' = 'Asia',
+  'S' = 'Europe',
+  'T' = 'Europe',
+  'U' = 'Europe',
+  'V' = 'Europe',
+  'W' = 'Europe',
+  'X' = 'Europe',
+  'Y' = 'Europe',
+  'Z' = 'Europe',
+  // @ts-ignore
+  '1' = 'North America',
+  // @ts-ignore
+  '2' = 'North America',
+  // @ts-ignore
+  '3' = 'North America',
+  // @ts-ignore
+  '4' = 'North America',
+  // @ts-ignore
+  '5' = 'North America',
+  // @ts-ignore
+  '6' = 'Oceania',
+  // @ts-ignore
+  '7' = 'Oceania',
+  // @ts-ignore
+  '8' = 'South America',
+  // @ts-ignore
+  '9' = 'South America',
+}
